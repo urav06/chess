f6c87be0b7f3a547e701f1ebefb3b1f6a82cdd5c
yash.baheti <yash.baheti@wakefit.co>
2023-06-04 17:06:12 +0530

    Fix-Fen

--- fen_utils.py
+++ fen_utils.py
@@ -1,22 +1,22 @@
 from typing import Dict
 
 from config import BOARD_SIZE
-from engine import Bishop, Board, King, Knight, Pawn, Piece, Queen, Rook
-from engine_types import Color, Location
+from engine import Board
+from engine_types import Color, Location, PieceType, Piece
 
-FEN_MAPPING: Dict[str, tuple[Color, type[Piece]]] = {
-    "p": (Color.BLACK, Pawn),
-    "r": (Color.BLACK, Rook),
-    "n": (Color.BLACK, Knight),
-    "b": (Color.BLACK, Bishop),
-    "q": (Color.BLACK, Queen),
-    "k": (Color.BLACK, King),
-    "P": (Color.WHITE, Pawn),
-    "R": (Color.WHITE, Rook),
-    "N": (Color.WHITE, Knight),
-    "B": (Color.WHITE, Bishop),
-    "Q": (Color.WHITE, Queen),
-    "K": (Color.WHITE, King)
+FEN_MAPPING: Dict[str, tuple[Color, PieceType]] = {
+    "p": (Color.BLACK, PieceType.PAWN),
+    "r": (Color.BLACK, PieceType.ROOK),
+    "n": (Color.BLACK, PieceType.KNIGHT),
+    "b": (Color.BLACK, PieceType.BISHOP),
+    "q": (Color.BLACK, PieceType.QUEEN),
+    "k": (Color.BLACK, PieceType.KING),
+    "P": (Color.WHITE, PieceType.PAWN),
+    "R": (Color.WHITE, PieceType.ROOK),
+    "N": (Color.WHITE, PieceType.KNIGHT),
+    "B": (Color.WHITE, PieceType.BISHOP),
+    "Q": (Color.WHITE, PieceType.QUEEN),
+    "K": (Color.WHITE, PieceType.KING)
 }
 
 INV_FEN_MAPPING = {v: k for k, v in FEN_MAPPING.items()}
@@ -29,31 +29,35 @@ def from_fen(board: Board, fen_string: str) -> None:
         placement_string = fen_data[0]
     elif len(fen_data) == 6:
         placement_string, color_data, _, _, _, _ = fen_data
-        active_color = Color.BLACK if color_data.lower() == "b" else Color.WHITE
+        active_color = Color.BLACK if color_data.lower() == "b"\
+            else Color.WHITE
         board.active_color = active_color
 
     placement_data = placement_string.split("/")
     if len(placement_data) != BOARD_SIZE:
-        raise ValueError("FEN Notation Error")
+        raise ValueError(f"FEN Notation Error in row {placement_data, placement_string}")
 
     for i, rank_data in enumerate(placement_data):
-        rank_repr = "".join(int(c)*"x" if c.isdigit() else c for c in rank_data)
+        rank_repr = "".join(int(c)*"x" if c.isdigit() else c
+                            for c in rank_data)
         for j, square_data in enumerate(rank_repr):
             if square_data != "x":
-                color, piece_type = FEN_MAPPING[square_data]
-                board.place_new(piece_type, color, Location(i, j))
+                piece = FEN_MAPPING[square_data]
+                board.place_new(Piece(color=piece[0],type=piece[1]), Location(i, j))
 
 
 def to_fen(board: Board) -> str:
     placement_string = ""
     for rank in board.board:
         empty_counter = 0
-        for square in rank:
-            if square is None:
+        for stack in rank:
+            if stack[3] == 0:
                 empty_counter += 1
             else:
-                piece = square
-                data = INV_FEN_MAPPING[(piece.color, type(piece))]
+                color = stack[1]
+                piece = stack[0]
+                # print("INV_FEN_MAPPING is",INV_FEN_MAPPING,"......",color,piece)
+                data = INV_FEN_MAPPING[(Color(color),PieceType(piece))]
                 if empty_counter != 0:
                     placement_string += f"{empty_counter}"
                     empty_counter = 0
--- test_engine.py
+++ test_engine.py
@@ -1,28 +1,58 @@
 import unittest
 
 from engine import Board
+from engine_types import Move, MoveType, Location, Color
+from fen_utils import to_fen, from_fen
+
 
 
 class TestEngine(unittest.TestCase):
 
+    TEST_EXECUTE_MOVE = {
+        "PASSING": [
+            ("rnbqkb1r/pppppppp/8/2n5/8/8/PPPPPPPP/RNBQKBNR", Move((6, 5), (4, 5), MoveType.PASSING), "rnbqkb1r/pppppppp/8/2n5/5P2/8/PPPPP1PP/RNBQKBNR"),
+            ("rnbqkb1r/pppppppp/8/2n5/8/8/PPPPPPPP/RNBQKBNR", Move((3,2),(5,3),MoveType.PASSING), "rnbqkb1r/pppppppp/8/8/8/3n4/PPPPPPPP/RNBQKBNR"),
+            ("rnbqkb1r/pppppppp/8/8/8/3n4/PPPPPPPP/RNBQKBNR",Move((6,2),(4,2),MoveType.PASSING),"rnbqkb1r/pppppppp/8/8/2P5/3n4/PP1PPPPP/RNBQKBNR")
+        ]
+
+    }
+
     def setUp(self) -> None:
-        self.board = Board()
+
+        board = Board()
+        self.board = board
         return super().setUp()
     
-    def test_board(self, d) -> None:
+    def test_board(self) -> None:
         board = Board()
         self.assertEqual(board.board.shape, (8, 8, 4))
         self.assertEqual(board.board.dtype, 'int8')
         self.assertEqual(board.board[0, 0, 0], 0)
 
     def test_execute_move(self) -> None:
-        pass
+        for test_case in self.TEST_EXECUTE_MOVE['PASSING']:
+            board = Board()
+            from_fen(board, test_case[0])
+            board.execute_move(test_case[1])
+            # print("In Test Case",test_case,board)
+            fen_string = to_fen(board)
+            print("Test case is",test_case)
+            print("Final output is",fen_string)
+            self.assertEqual(fen_string, test_case[2])
 
     def test_get_legal_moves(self) -> None:
-        pass
+        for test_case in self.TEST_EXECUTE_MOVE['PASSING']:
+            board = Board()
+            from_fen(board, test_case[0])
+            board.generate_possible_moves(Color.WHITE)
+            # print("In Test Case",test_case,board)
+            fen_string = to_fen(board)
+            print("Test case is", test_case)
+            print("Final output is", fen_string)
+            self.assertEqual(fen_string, test_case[2])
 
     def test_pawn_moves(self) -> None:
-        self.board 
+        # self.board
         pass
 
     def test_knight_moves(self) -> None:
