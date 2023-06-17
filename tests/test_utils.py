21fbe676b80ac9e0033cfb31c5c6655adecb317a
Urav <urav@urav.xyz>
2023-06-17 18:14:51 +0530

    renaming board_dump, updating game locic in test utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 import numpy.testing as np_testing
 
-from engine import Board
+from engine import Board, Game
 from engine.fen_utils import from_fen, to_fen
 
 
@@ -13,7 +13,7 @@ class TestUtils(unittest.TestCase):
     INVALID_RANKS_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR/8"  # 9 ranks
 
     def test_to_fen_classic(self) -> None:
-        board_arr = np.load("tests/data/board_dump.npy")
+        board_arr = np.load("tests/data/classic_board_dump.npy")
         expected_fen = self.CLASSIC_FEN
         board = Board()
         board.board = board_arr
@@ -21,16 +21,16 @@ class TestUtils(unittest.TestCase):
 
     def test_from_fen_classic(self) -> None:
         fen = self.CLASSIC_FEN
-        expected_board_arr = np.load("tests/data/board_dump.npy")
-        board = Board()
-        from_fen(board, fen)
-        np_testing.assert_array_equal(board.board, expected_board_arr)
+        expected_board_arr = np.load("tests/data/classic_board_dump.npy")
+        game = Game()
+        from_fen(fen, game)
+        np_testing.assert_array_equal(game.board.board, expected_board_arr)
 
     def test_from_fen_invalid_ranks(self) -> None:
         fen_string = self.INVALID_RANKS_FEN
-        board = Board()
+        game = Game()
         with self.assertRaises(ValueError):
-            from_fen(board, fen_string)
+            from_fen(fen_string, game)
 
 
 if __name__ == "__main__":
