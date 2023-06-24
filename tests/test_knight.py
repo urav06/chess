7ed4713b44b18d4af7cd2fe309559b739756d057
yash.baheti <yash.baheti@wakefit.co>
2023-06-24 19:38:47 +0530

    Knight-Cases-3

--- tests/test_knight.py
+++ tests/test_knight.py
@@ -3,7 +3,7 @@ Unittests for knight movement logic
 """
 import unittest
 from engine.game import Game
-from engine.types import Piece, PieceType, Color,Move,MoveType,Location 
+from engine.types import Piece, PieceType, Color,Move,MoveType,Location
 
 
 class TestKnight(unittest.TestCase):
@@ -29,7 +29,7 @@ class TestKnight(unittest.TestCase):
         self.default_game.add_piece(location=Location(1, 1), piece=Piece(type=PieceType.KNIGHT, color=Color.BLACK))
         calculated_legal_moves = set(list(self.default_game.legal_moves(color=Color.BLACK)))
         print("Calculated Legal moves is", calculated_legal_moves)
-        assert self.assertSetEqual(calculated_legal_moves, expected_legal_moves)
+        self.assertSetEqual(calculated_legal_moves, expected_legal_moves)
 
     def test_all_center_moves(self):
         expected_legal_move_list = [
@@ -52,8 +52,29 @@ class TestKnight(unittest.TestCase):
         calculate_legal_moves = set(list(self.default_game.legal_moves(color=Color.WHITE)))
         self.assertSetEqual(expected_legal_moves, calculate_legal_moves)
 
-    def test_check_center_moves(self):
+    def test_capture_knight_moves(self):
         expected_legal_move_list = [
+            Move(start=Location(0, 4), end=Location(1, 3), type=MoveType.PASSING, target=None),
+            Move(start=Location(0, 4), end=Location(1, 5), type=MoveType.PASSING, target=None),
+            Move(start=Location(0, 4), end=Location(1, 4), type=MoveType.PASSING, target=None),
+            Move(start=Location(3, 2), end=Location(2, 0), type=MoveType.PASSING, target=None),
+            Move(start=Location(3, 2), end=Location(1, 1), type=MoveType.CAPTURE, target=None),
+            Move(start=Location(3, 2), end=Location(2, 4), type=MoveType.CAPTURE, target=None),
+            Move(start=Location(3, 2), end=Location(4, 0), type=MoveType.PASSING, target=None),
+            Move(start=Location(3, 2), end=Location(5, 1), type=MoveType.PASSING, target=None),
+            Move(start=Location(3, 2), end=Location(1, 3), type=MoveType.PASSING, target=None),
+            Move(start=Location(3, 2), end=Location(5, 3), type=MoveType.PASSING, target=None),
+            Move(start=Location(3, 2), end=Location(4, 4), type=MoveType.PASSING, target=None),
+        ]
+        expected_legal_moves = set(expected_legal_move_list)
+        self.default_game.add_piece(location=Location(3, 2), piece=Piece(type=PieceType.KNIGHT, color=Color.BLACK))
+        self.default_game.add_piece(location=Location(2, 4), piece=Piece(type=PieceType.KNIGHT, color=Color.WHITE))
+        self.default_game.add_piece(location=Location(1, 1), piece=Piece(type=PieceType.KNIGHT, color=Color.WHITE))
+        calculate_legal_moves = set(list(self.default_game.legal_moves(color=Color.BLACK)))
+        self.assertSetEqual(expected_legal_moves, calculate_legal_moves)
+
+        def test_check_center_moves(self):
+            expected_legal_move_list = [
             Move(start=Location(0, 4), end=Location(0, 5), type=MoveType.PASSING, target=None),
             Move(start=Location(0, 4), end=Location(0, 3), type=MoveType.PASSING, target=None),
             Move(start=Location(0, 4), end=Location(1, 3), type=MoveType.PASSING, target=None),
@@ -62,11 +83,7 @@ class TestKnight(unittest.TestCase):
             Move(start=Location(3, 3), end=Location(5, 4), type=MoveType.PASSING, target=None),
         ]
         expected_legal_moves = set(expected_legal_move_list)
-        self.default_game.add_piece(location=Location(3, 3), piece=Piece(type=PieceType.KNIGHT, color=Color.BLACK))
+        self.default_game.add_piece(location=Location(3, 2), piece=Piece(type=PieceType.KNIGHT, color=Color.BLACK))
         self.default_game.add_piece(location=Location(6, 4), piece=Piece(type=PieceType.ROOK, color=Color.WHITE))
         calculate_legal_moves = set(list(self.default_game.legal_moves(color=Color.BLACK)))
         self.assertSetEqual(expected_legal_moves, calculate_legal_moves)
-
-
-
-    
