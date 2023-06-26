ce32b2068a3cb91a24e6b6916a62b46479b73919
Urav <urav06@gmail.com>
2023-06-26 22:27:04 +0530

    ghu + profiling

--- profiling.py
+++ profiling.py
@@ -6,15 +6,15 @@ import cProfile
 import os
 import pstats
 import random
-import subprocess
 
 from dotenv import load_dotenv
 
 from engine import Game, Move
 from engine.fen_utils import from_fen
+from github_action_utils import GithubActionUtils as gau
 
 PER_GAME_MOVE_LIMIT = 200
-GAME_COUNT = 4
+GAME_COUNT = 25
 
 def random_game(limit: int = PER_GAME_MOVE_LIMIT) -> bool:
     game = Game()
@@ -24,48 +24,43 @@ def random_game(limit: int = PER_GAME_MOVE_LIMIT) -> bool:
         if not moves:
             if game.is_in_check(color=game.active_color):
                 return "CHECKMATE"
-            else:
-                return "STALEMATE"
+            return "STALEMATE"
         move: Move = random.choice(moves)
         game.execute_move(move)
         game.active_color = ~game.active_color
     return f"EXHAUSTED {limit} Moves"
 
-def run_games(count: int = GAME_COUNT, status = None) -> None:
+def run_games(count: int = GAME_COUNT, game_results = None) -> None:
     for _ in range(count):
         ret = random_game(PER_GAME_MOVE_LIMIT)
         if os.getenv("ENVIRONMENT") != "GITHUB":
             print(ret)
-        if ret not in status:
-            status[ret] = 0
-        status[ret] += 1
-    return status
+        if ret not in game_results:
+            game_results[ret] = 0
+        game_results[ret] += 1
+    return game_results
 
-def github_summary(stat: pstats.Stats, status: dict) -> None:
-    github_markdown_line("Game Statuses:")
-    for key, value in status.items():
-        github_markdown_line(f"{key}: {value}")
-    github_markdown_line("")
-    github_markdown_line(f"Average Per Game Time: {stat.total_tt/GAME_COUNT}")
+def run_profiler() -> None:
+    profiler = cProfile.Profile()
+    game_results = {}
+    output = profiler.run("run_games(game_results=game_results)")
+    stats = pstats.Stats(output)
+    stats.print_stats()
+    summary(stats, game_results)
 
+def summary(stat: pstats.Stats, status: dict) -> None:
+    if os.getenv("ENVIRONMENT") == "GITHUB":
+        gau.markdown_line("### Game Statuses ###")
+        gau.tabulate(["Status", "Count"], [[key, value] for key, value in status.items()])
+        gau.markdown_line("")
+        gau.markdown_line(f"Average Per Game Time: {stat.total_tt/GAME_COUNT}")
+    else:
+        print("GAME STATUS:")
+        for key, value in status.items():
+            print(f"{key}: {value}")
+        print(f"AVERAGE PER GAME TIME: {stat.total_tt/GAME_COUNT}")
 
-def github_markdown_line(line: str) -> None:
-    subprocess.run(
-        f"echo \"{line}\" >> $GITHUB_STEP_SUMMARY",
-        shell=True,
-        check=True
-    )
 
 if __name__ == "__main__":
     load_dotenv()
-    profiler = cProfile.Profile()
-    status = {}
-    op = profiler.run("run_games(status=status)")
-    stats = pstats.Stats(op)
-    op.print_stats()
-    print(f"AVERAGE PER GAME TIME: {stats.total_tt/GAME_COUNT}")
-    print("GAME STATUS:")
-    for key, value in status.items():
-        print(f"{key}: {value}")
-    if os.getenv("ENVIRONMENT") == "GITHUB":
-        github_summary(stats, status)
+    run_profiler()
