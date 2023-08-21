6efa4a07ffc4b7cd892cd830411cb36a43b5eb92
yash.baheti <yash.baheti@wakefit.co>
2023-08-22 00:00:28 +0530

    ..

--- bots/search_and_eval.py
+++ bots/search_and_eval.py
@@ -1,50 +1,56 @@
-from  engine import Game,Board,Color
 from bots.model import sigmoid
-
-def evaluation_by_heuristics(game:Game)->int:
-  return (
-    sigmoid(
-      len(game.filter_color_pieces(color=Color.WHITE))
-      - len(game.filter_color_pieces(color=Color.BLACK))
-    )-0.5
-  )*10
+from engine import Game, Color, Board
 
 
-class  SearchTree:
+def evaluation_by_heuristics(game: Game) -> int:
+    return (
+        sigmoid(
+            len(game.filter_color_pieces(color=Color.WHITE))
+            - len(game.filter_color_pieces(color=Color.BLACK))
+        )
+        - 0.5
+    ) * 10
 
-  def __init__(self):
-    self.alpha = float("inf")
-    self.beta = float("-inf")
-    # self.base_values =[]
-    # self.cur_val = 0 
 
-  def search_and_evaluation(self,game:Game,depth:int,state:bool): # state is 0 for max and 1 for min
-      if depth==0:
-          # self.cur_val = (self.cur_val + 1) %4
-          x=evaluation_by_heuristics(game)
-          # x = self.base_values[self.cur_val]
-          return x
-      else:
-          all_legal_moves = game.legal_moves()
-          out_value = float("inf")
-          if state:
-            out_value = float("-inf")
+class SearchTree:
+    def __init__(self):
+        self.alpha = float("inf")
+        self.beta = float("-inf")
+        # self.base_values =[]
+        # self.cur_val = 0
 
-          for (i,current_move) in enumerate(all_legal_moves):
-                  print(depth,i)
-                  print("Alpha and beta values are",self.alpha,self.beta)
-                  value=self.search_and_evaluation(game.seek_move(current_move),depth-1,state= not state)
-                  if state:
-                    if  value >self.alpha:
-                      break
+    def search_and_evaluation(
+        self, game: Game, depth: int, state: bool
+    ):  # state is 0 for max and 1 for min
+        if depth == 0:
+            # self.cur_val = (self.cur_val + 1) %4
+            x = evaluation_by_heuristics(game)
+            # x = self.base_values[self.cur_val]
+            return x
+        else:
+            all_legal_moves = game.legal_moves()
+            out_value = float("inf")
+            if state:
+                out_value = float("-inf")
+
+            for i, current_move in enumerate(all_legal_moves):
+                print(depth, i)
+                print("Alpha and beta values are", self.alpha, self.beta)
+                value = self.search_and_evaluation(
+                    game.seek_move(current_move), depth - 1, state=not state
+                )
+                if state:
+                    if value > self.alpha:
+                        ...
                     else:
-                       self.alpha = value
+                        self.alpha = value
 
-                  else: 
+                else:
                     if value < self.beta:
-                      break
-                    else: 
-                       self.beta = value 
-                  out_value =  max(out_value,value) if state else min(out_value,value)
+                        ...
+                    else:
+                        self.beta = value
+                out_value = max(out_value, value) if state else min(out_value,
+                                                                    value)
 
-          return  out_value
+            return out_value
