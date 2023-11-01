4d1ae8c5fdc1f1a859ed7c6891a990ac93400ff4
yash.baheti <yash.baheti@wakefit.co>
2023-11-01 13:56:52 +0530

    Remove Logs in Search and Eval

--- bots/min_max/search_and_eval.py
+++ bots/min_max/search_and_eval.py
@@ -1,7 +1,5 @@
 from typing import Any
-
 from numpy import ndarray, dtype, floating
-
 from bots.model import sigmoid
 from engine import Game, Color, Board
 
@@ -29,10 +27,8 @@ class SearchTree:
     ):  # state is 0 for max and 1 for min
 
         self.count = self.count+1
-        color= Color.WHITE if state else Color.BLACK
+        color = Color.WHITE if state else Color.BLACK
         if depth == 0:
-            print("Board At Last Stage is")
-            print(game.board)
             x = evaluation_by_heuristics(game)
             return x
         else:
@@ -43,32 +39,23 @@ class SearchTree:
 
             for i, current_move in enumerate(all_legal_moves):
                 if state:
-                    if score in (float("inf"),float("-inf")):
+                    if score in (float("inf"), float("-inf")):
                         pass
                     elif score >= self.alpha:
-                        print("Alpha Values are", score, self.alpha)
                         break
                     else:
                         self.alpha = score
                 else:
-                    if score in (float("inf"),float("-inf")):
+                    if score in (float("inf"), float("-inf")):
                         pass
                     elif score <= self.beta:
-                        print("Beta Values are", score, self.beta)
                         break
                     else:
                         self.beta = score
-                print(depth,i)
-                # print("Alpha and beta values are", self.alpha, self.beta)
                 child_score = self.search_and_evaluation(
                     game.seek_move(current_move), depth - 1, state=not state
                 )
-                print("Move is",current_move.start,current_move.end,current_move.target,current_move.type,
-                      "Piece is",game.board.get_piece(current_move.start))
-                print("Child Score is",child_score)
-                print("Board is")
-                print(game.board)
-                score = max(score, child_score) if state else min(score,
-                                                                    child_score )
+
+                score = max(score, child_score) if state else min(score, child_score)
 
             return score
