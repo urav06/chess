b899c4b0a1d9f5db399f2831d815bd9cb0bb10ee
yash.baheti <yash.baheti@wakefit.co>
2023-11-01 15:10:47 +0530

    Add To test min max search_and_eval bot

---
+++ bots/min_max_pro/__init__.py
@@ -0,0 +1,3 @@
+from bots.min_max_pro.search_and_eval import SearchTree
+
+__all__ = ["SearchTree"]
--- bots/min_max_pro/search_and_eval.py
+++ bots/min_max_pro/search_and_eval.py
@@ -1,13 +1,13 @@
-from typing import Any
-from numpy import ndarray, dtype, floating
-from bots.model import sigmoid
-from engine import Game, Color, Board
+
+from bots.neural_network.model import sigmoid
+from engine import Game, Color, Move
+from bots.basebot import BaseBot
 
 
 def evaluation_by_heuristics(game: Game):
 
-    param_a = len(game.filter_color_pieces(color=Color.WHITE))
-    param_b = len(game.filter_color_pieces(color=Color.BLACK))
+    param_a = len(game.board.get_pieces(color=Color.WHITE))
+    param_b = len(game.board.get_pieces(color=Color.BLACK))
     return (
         sigmoid(
             (param_a - param_b) * (32/(param_a + param_b))
@@ -16,11 +16,21 @@ def evaluation_by_heuristics(game: Game):
     ) * 10
 
 
-class SearchTree:
-    def __init__(self):
+class SearchTree(BaseBot):
+    def __init__(self, color):
+        super().__init__(color)
         self.alpha = float("inf")
         self.beta = float("-inf")
         self.count = 0
+
+    def select_move(self, game: Game) -> Move | None:
+        super().select_move(game)
+        move, score = self.search_and_evaluation(
+            game=game,
+            depth=10,
+            state=(self.color == Color.WHITE)
+        )
+        return move
 
     def search_and_evaluation(
         self, game: Game, depth: int, state: bool
@@ -28,9 +38,10 @@ class SearchTree:
 
         self.count = self.count+1
         color = Color.WHITE if state else Color.BLACK
+        final_move = None
         if depth == 0:
             x = evaluation_by_heuristics(game)
-            return x
+            return None, x
         else:
             all_legal_moves = game.legal_moves(color=color)
             score = float("inf")
@@ -52,10 +63,12 @@ class SearchTree:
                         break
                     else:
                         self.beta = score
-                child_score = self.search_and_evaluation(
+                _, child_score = self.search_and_evaluation(
                     game.seek_move(current_move), depth - 1, state=not state
                 )
-
+                check_score = score
                 score = max(score, child_score) if state else min(score, child_score)
+                if check_score != score:
+                    final_move = current_move
 
-            return score
+            return final_move, score
